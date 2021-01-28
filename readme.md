ee7d2e58f8cad3d2b06dbbc0d4c72e7088a39ef3
MatsM16 <matdyr23@gmail.com>
2021-01-28 22:36:10 +0100

    Initial build

---
+++ readme.md
@@ -0,0 +1,10 @@
+# DDevJS
+This will be the successor to DDevLib.
+
+**This project is WIP**
+
+**TODO:**
+* Write theme-able scss components
+* Write web components complementing the styles
+* Make adding more projects easier
+* Make better github page
---
+++ source/gulpfile.js
@@ -0,0 +1,122 @@
+const { src, dest, series, parallel} = require("gulp");
+const compile_sass = require("gulp-sass");
+const compile_typescript = require("gulp-typescript");
+const minify_css = require("gulp-clean-css");
+const minify_js = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const del = require("del");
+const mergeStreams = require("merge-stream");
+const zip = require("gulp-zip");
+
+const path =
+{
+    source(project, ...extensions)
+    {
+        if (extensions.length === 0)
+            extensions.push("*");
+        return extensions.map(extension => `../source/${project}/**/*.${extension}`);
+    },
+
+    build(fileName)
+    {
+        return `../build/${fileName || ""}`;
+    }
+}
+
+const getBuild = 
+{
+    scss(project, outFile)
+    {
+        const dev = 
+            src(path.source(project, "sass", "scss"))
+            .pipe(sourcemaps.init())
+            .pipe(compile_sass())
+            .pipe(src(path.source(project, "css")))
+            .pipe(concat(`dev.${outFile}`))
+            .pipe(minify_css())
+            .pipe(sourcemaps.write(path.build()))
+            .pipe(dest(path.build()))
+
+        const prod = 
+            src(path.source(project, "sass", "scss"))
+            .pipe(compile_sass())
+            .pipe(src(path.source(project, "css")))
+            .pipe(concat(outFile))
+            .pipe(minify_css())
+            .pipe(dest(path.build()))
+
+        return mergeStreams(dev,prod);
+    },
+
+    ts(project, outFile)
+    {
+        const compile_dev = compile_typescript.createProject("./tsconfig.json");
+        const compile_prod = compile_typescript.createProject("./tsconfig.json");
+
+        const dev = 
+            src(path.source(project, "ts"))
+            .pipe(sourcemaps.init())
+            .pipe(compile_dev())
+            .pipe(src(path.source(project, "js")))
+            .pipe(concat(`dev.${outFile}`))
+            .pipe(minify_js())
+            .pipe(sourcemaps.write(path.build()))
+            .pipe(dest(path.build()))
+
+        const prod = 
+            src(path.source(project, "ts"))
+            .pipe(compile_prod())
+            .pipe(src(path.source(project, "js")))
+            .pipe(concat(outFile))
+            .pipe(minify_js())
+            .pipe(dest(path.build()))
+
+        return mergeStreams(dev,prod);
+    }
+}
+
+const tasks = 
+{
+    "pre:clean": function ()
+    {
+        return del(path.build("**"), {force: true});
+    },
+
+    "post:clean": function ()
+    {
+        return del(["../build/**", "!../build/**.zip"], {force: true});
+    },
+
+    "post:zip": function ()
+    {
+        return mergeStreams(
+            src("../build/dev.**").pipe(zip("dev.ddevjs.zip")).pipe(dest("../build/")),
+            src("../build/ddevjs.**").pipe(zip("ddevjs.zip")).pipe(dest("../build/"))
+        )
+    },
+
+    "build:DDevJS.Styles": function ()
+    {
+        return getBuild.scss("DDevJS.Styles", "ddevjs.css");
+    },
+
+    "build:DDevJS.Components": function ()
+    {
+        return getBuild.ts("DDevJS.Components", "ddevjs.components.js");
+    }
+}
+
+exports.clean = tasks["clean"];
+
+exports.build = series(
+    tasks["pre:clean"], 
+
+    parallel(
+        tasks["build:DDevJS.Styles"],
+        tasks["build:DDevJS.Components"]
+    ),
+
+    tasks["post:zip"],
+    tasks["post:clean"]
+);
